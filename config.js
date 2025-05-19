a1ec427e6393a362b639da5e53b6a8226c2b4a7e
Rovic-Pet <rovicpet974@gmail.com>
2025-05-19 23:31:49 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 require('dotenv').config();
 
 module.exports = {
-    prefix: '!',
+    prefix: '', // Can be '', or any other single prefix
     owners: ['254742063632'],
-    AUTO_READ_MESSAGES: process.env.AUTO_READ, 
+    AUTO_READ_MESSAGES: process.env.AUTO_READ || 'off',
     sessionBase64: process.env.SESSION_BASE64 || ''
 };
