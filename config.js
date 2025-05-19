cd84e2fbe475acceddb7374f3529bacc44961fdb
Rovic-Pet <rovicpet974@gmail.com>
2025-05-19 13:03:54 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
     prefix: '!',
     owners: ['254742063632'],
-    sessionBase64: process.env.SESSION_BASE64 || '' // Optional: or hardcode your sessionBase64 here
+    sessionBase64: process.env.SESSION_BASE64 || ''
 };
