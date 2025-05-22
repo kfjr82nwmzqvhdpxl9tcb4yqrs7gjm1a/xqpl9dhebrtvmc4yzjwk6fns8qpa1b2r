b4857651254395acb94d5d10c1d62db3a33a7254
Rovic-Pet <rovicpet974@gmail.com>
2025-05-22 17:44:48 +0300

    Update index.js man

--- commands/index.js
+++ commands/index.js
@@ -1,16 +1,9 @@
 const fs = require('fs');
 const path = require('path');
 
-const commandFiles = fs.readdirSync(__dirname).filter(file => file.endsWith('.js') && file !== 'index.js');
-const commands = [];
+const commandFiles = fs.readdirSync(__dirname)
+    .filter(file => file.endsWith('.js') && file !== 'index.js');
 
-for (const file of commandFiles) {
-    const commandModule = require(path.join(__dirname, file));
-    if (Array.isArray(commandModule)) {
-        commands.push(...commandModule);
-    } else {
-        commands.push(commandModule);
-    }
-}
+const commands = commandFiles.flatMap(file => require(path.join(__dirname, file)));
 
 module.exports = commands;
