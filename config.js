333d74a8835ee675b31e1e83b8271853070f7f93
Rovic-Pet <rovicpet974@gmail.com>
2025-05-25 20:40:20 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 require('dotenv').config();
 
 module.exports = {
-    prefixes: process.env.PREFIX !== undefined
-        ? process.env.PREFIX.split(',')
-        : [],
+    prefixes: process.env.PREFIX
+        ? process.env.PREFIX.split(',').map(p => p.trim()) // allows "", "!", ".", etc.
+        : [''], // fallback: no prefix allowed
     owners: ['254742063632', '254757835036'],
     MODE: process.env.MODE || 'private',
     AUTO_VIEW_STATUS: process.env.AUTO_READ_STATUS === 'on',
