ddbaa39ceb22c67c7b281b074d912f3f756eb98f
Rovic-Pet <rovicpet974@gmail.com>
2025-05-27 12:23:04 +0300

    Update config.js

--- config.js
+++ config.js
@@ -4,12 +4,20 @@ module.exports = {
     prefixes: process.env.PREFIX
         ? process.env.PREFIX.split(',').map(p => p.trim())
         : [''],
+        
     owners: ['254742063632', '254757835036'],
+
     MODE: process.env.MODE || 'private',
     ANTICALL: process.env.ANTICALL || 'on',
+
     AUTO_VIEW_STATUS: process.env.AUTO_READ_STATUS === 'on',
     AUTO_LIKE: process.env.AUTO_LIKE === 'on',
     AUTO_READ_MESSAGES: process.env.AUTO_READ_DM === 'on',
+
     sessionBase64: process.env.SESSION || '',
-    timezone: 'Africa/Nairobi'
+    
+    timezone: 'Africa/Nairobi',
+
+    PRESENCE_DM: process.env.PRESENCE_DM || 'typing',
+    PRESENCE_GROUP: process.env.PRESENCE_GROUP || 'recording'
 };
