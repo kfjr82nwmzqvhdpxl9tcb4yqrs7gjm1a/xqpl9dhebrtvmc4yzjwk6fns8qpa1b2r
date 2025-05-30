b5d8190db135509994bd50e6c19537cbb3e09e7a
Rovic-Pet <rovicpet974@gmail.com>
2025-05-30 14:13:23 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-Oconst express = require('express');
+const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 app.get('/', (req, res) => res.send('WhatsApp Bot is running!'));
