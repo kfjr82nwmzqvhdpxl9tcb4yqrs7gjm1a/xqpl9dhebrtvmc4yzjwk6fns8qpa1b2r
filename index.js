e160933604330e8172ccd8686377f3f4ab72770f
Rovic-Pet <rovicpet974@gmail.com>
2025-06-09 20:36:26 +0300

    Update index.js

--- index.js
+++ index.js
@@ -309,7 +309,7 @@ The following message was deleted:`,
         } catch (err) {
             console.error('❌ Failed to like status:', err);
         }
-    }
+    
 
         const text = m?.conversation || m?.extendedTextMessage?.text || m?.imageMessage?.caption || m?.videoMessage?.caption || '';
         if (!text) return;
