1e3428bd5691b45ec84100a3cc37f77f5894bdbe
Rovic-Pet <rovicpet974@gmail.com>
2025-07-08 15:15:30 +0300

    Update king.js

--- king.js
+++ king.js
@@ -507,12 +507,12 @@ function normalizeId(jid) {
   return jid?.split('@')[0];
 }
 
-const normalizedAdminNumbers = groupAdmins.map(id => getUserNumber(id));
+const normalizedAdmins = groupAdmins.map(normalizeId);
 const senderNorm = normalizeId(senderJidRaw);
-const botNorm = normalizedAdminNumbers(king.user.id);
+const botNorm = normalizeId(king.user.id);
 
-const isAdmin = normalizedAdminNumbers.includes(senderNumber);
-const isBotAdmin = normalizedAdminNumbers.includes(getUserNumber(king.user.id));
+const isAdmin = normalizedAdmins.includes(senderNorm);
+const isBotAdmin = normalizedAdmins.includes(botNorm);
 const senderIdNormalized = normalizeJid(senderJid);
 const botIdNormalized = normalizeJid(king.user.id);
 
@@ -548,8 +548,9 @@ console.log('✅ Allowed Users:', global.ALLOWED_USERS);
       }, { quoted: msg });
     }
 
+// ☠️☠️☠️💔 BE BACK FRANCE 
   
-  if (command.adminOnly || command.botAdminOnly) {
+  /*if (command.adminOnly || command.botAdminOnly) {
   if (!isBotAdmin) {
     return king.sendMessage(fromJid, {
       text: '❗ I need to be admin to run this command.',
@@ -560,7 +561,7 @@ console.log('✅ Allowed Users:', global.ALLOWED_USERS);
       text: '⛔ This command is restricted to group admins.',
     }, { quoted: msg });
   }
-  }
+}*/
 
     try {
       await command.execute(king, msg, args, fromJid, allCommands);
