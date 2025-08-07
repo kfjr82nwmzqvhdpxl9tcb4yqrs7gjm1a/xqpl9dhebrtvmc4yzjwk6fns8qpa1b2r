7f9cd8ccc60db2a1a7a9a2b6284eff3666eab0b4
Rovic-Pet <rovicpet974@gmail.com>
2025-08-07 19:35:54 +0300

    Update Genius.js

--- france/Genius.js
+++ france/Genius.js
@@ -1,14 +1,12 @@
-// france/genius.js
 const axios = require('axios');
 const cheerio = require('cheerio');
 
 const GENIUS_HEADERS = {
-  'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36',
+  'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 15_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148',
   'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
-  'Accept-Language': 'en-US,en;q=0.5',
+  'Accept-Language': 'en-US,en;q=0.9',
   'Referer': 'https://genius.com/',
-  'Connection': 'keep-alive',
-  'Upgrade-Insecure-Requests': '1',
+  'Connection': 'keep-alive'
 };
 
 async function searchGenius(query) {
