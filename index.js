500ddb3871c6a6420869eeb90a97824ba75436ef
France King <151246926+franceking1@users.noreply.github.com>
2025-08-09 01:47:50 +0300

    Update index.js

--- index.js
+++ index.js
@@ -5,5 +5,5 @@ const PORT = process.env.PORT || 3000;
 app.get('/', (req, res) => res.send('WhatsApp Bot is running!'));
 app.listen(PORT, () => console.log(`Web server running on port ${PORT}`));
 
-require('./db-init')();
+
 require('./king');
