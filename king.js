8baacacb0f1bd63e569af6dab603813521e1ca14
Rovic-Pet <rovicpet974@gmail.com>
2025-10-28 11:51:44 +0300

    Update king.js

--- king.js
+++ king.js
@@ -100,8 +100,8 @@ async function startBot() {
       }
     }
 if (connection === 'open') {
-await king.newsletterFollow("120363238139244263@newsletter");
-await king.newsletterUnmute("120363238139244263@newsletter")
+/*await king.newsletterFollow("120363238139244263@newsletter");
+await king.newsletterUnmute("120363238139244263@newsletter")*/
     global.KING_LID = king.user.id;
 
 const lidRaw = king.user.id.replace('@lid', '');
